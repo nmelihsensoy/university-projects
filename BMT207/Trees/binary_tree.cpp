48914bd6d533a081e69d089e53ea203b144d0e92
Nuri Melih Sensoy <nmelihsensoy@gmail.com>
2019-07-11 01:39:32 +0300

    added tree node delete & comments

--- BMT207/Trees/binary_tree.cpp
+++ BMT207/Trees/binary_tree.cpp
@@ -33,21 +33,21 @@ btree *add(btree *r, int x){
     return r;
 }
 
-void traverse_inorder(btree *r){
+void traverse_inorder(btree *r){ //Left Root Right
     if(r == NULL) return;
     traverse_inorder(r->left);
     cout<<r->data<<", ";
     traverse_inorder(r->right);
 }
 
-void traverse_preorder(btree *r){
+void traverse_preorder(btree *r){ //Root Left Right
     if(r == NULL) return;
     cout<<r->data<<", ";
     traverse_preorder(r->left);
     traverse_preorder(r->right);
 }
 
-void traverse_postorder(btree *r){
+void traverse_postorder(btree *r){ // Left Right Root
     if(r == NULL) return;
     traverse_postorder(r->left);
     traverse_postorder(r->right);
@@ -59,9 +59,19 @@ btree *max(btree *r){
     return r;
 }
 
+btree *max_rec(btree *r){
+    if(r->right == NULL) return r;
+    return max_rec(r->right);
+}
+
 btree *min(btree *r){
     while(r->left != NULL) r = r->left;
     return r;
+}
+
+btree *min_rec(btree *r){
+    if(r->left == NULL) return r;
+    return min_rec(r->left);
 }
 
 int count(btree *r){
@@ -85,25 +95,29 @@ btree *search2(btree *r, int x){
 
 btree *del_leaf(btree *r, int x){
     if(r == NULL) return r;
-    r->left = del_leaf(r->left, x);
-    r->right = del_leaf(r->right, x);
     if(r->data == x && r->left == NULL && r->right == NULL){
-        free(r);
         return NULL;
-    }
+    }else if(r->data < x) return del_leaf(r->right, x);
+    else del_leaf(r->left, x);
     return r;
 }
 
-
-btree *del_leaf2(btree *r, int x){
+btree *del(btree *r, int x){
     if(r == NULL) return r;
-    if(r->data == x){
-        if(r->left == NULL && r->right == NULL){
-            free(r);
+    if(r->data == x){ 
+        if(r->left == NULL && r->right == NULL){ //node is a leaf
             return NULL;
         }
-    }else if(r->data < x) r->right = del_leaf2(r->right, x);
-    else r->left = del_leaf2(r->left, x);
+        if(r->left == NULL){ //node is a parent and has child on right node
+            r->data = min(r->right)->data; //swap data with min node
+            r->right = del(r->right, min(r->right)->data); //recurse until find x or finish tree
+            return r;
+        }//node is a parent and has child on left node
+        r->data = max(r->left)->data; //swap data
+        r->left = del(r->left, max(r->left)->data); // recurse until find x or finish tree
+        return r;
+    }else if(r->data < x) r->right = del(r->right, x); //if x > current node data continue to right
+    else r->left = del(r->left, x);//if x < current node data continue to left
     return r;
 }
 
@@ -111,24 +125,24 @@ int main(){
 
     btree *b1 = NULL;
     b1 = add(b1, 15);
-    add(b1, 16);
-    add(b1, 20);
-    add(b1, 18);
-    add(b1, 23);
-    add(b1, 5);
-    add(b1, 12);
-    add(b1, 13);
-    add(b1, 3);
-    add(b1, 10);
-    add(b1, 6);
-    add(b1, 7);
+    b1 = add(b1, 6);
+    b1 = add(b1, 3);
+    b1 = add(b1, 2);
+    b1 = add(b1, 4);
+    b1 = add(b1, 7);
+    b1 = add(b1, 13);
+    b1 = add(b1, 18);
+    b1 = add(b1, 17);
+    b1 = add(b1, 20);
     traverse_inorder(b1);
     cout<<endl;
-    del_leaf2(b1, 13);
+    b1 = del(b1, 20);
+    b1 = del(b1, 13);
     traverse_inorder(b1);
     cout<<endl;
     cout<<"Count:"<<count(b1)<<" Min:"<<min(b1)->data<<" Max:"<<max(b1)->data<<endl;
-    if(search(b1, 13) != NULL) cout<<"Found!"<<endl;
-    else cout<<"Didn't found."<<endl;
+    int s = 13;
+    if(search(b1, s) != NULL) cout<<s<<": found!"<<endl;
+    else cout<<s<<" didn't found."<<endl;
     return 0;
 }
